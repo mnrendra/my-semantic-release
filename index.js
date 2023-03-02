785e8c1c9a573e261efd6b82732a9b1137fda421
Muhammad Rendra <muhammadnurrendra@gmail.com>
2023-03-03 00:18:00 +0700

    feat(index.js): add `minus` method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   hello: () => console.log('hello world!'),
-  plus: (a, b) => console.log(a + b)
+  plus: (a, b) => console.log(a + b),
+  minus: (a, b) => console.log(a - b),
 }
